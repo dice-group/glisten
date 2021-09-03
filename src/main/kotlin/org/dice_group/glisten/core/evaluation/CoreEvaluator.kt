4a7795ad572d541e17ddd2ee40d3f6a2003bdec0
TortugaAttack <lixiconrads@gmail.com>
2021-09-03 20:06:24 +0200

    start of CoreEvaluatorTest and added better ROC curve for final ROC

--- src/main/kotlin/org/dice_group/glisten/core/evaluation/CoreEvaluator.kt
+++ src/main/kotlin/org/dice_group/glisten/core/evaluation/CoreEvaluator.kt
@@ -12,6 +12,7 @@ import org.dice_group.glisten.core.scorer.Scorer
 import org.dice_group.glisten.core.utils.DownloadUtils
 import org.dice_group.glisten.core.utils.RDFUtils
 import java.io.File
+import java.io.IOException
 import java.net.URL
 import kotlin.jvm.Throws
 
@@ -34,8 +35,9 @@ class CoreEvaluator(private val conf: Configuration, private val rdfEndpoint: St
      *
      * @param zipDest the destination folder to download the zip file to
      * @throws ZipException if the downloaded file is not a zip file
+     * @throws IOException If the links url in the configuration doesn't exist
      */
-    @Throws(ZipException::class)
+    @Throws(ZipException::class, IOException::class)
     fun init(zipDest: String){
         val zipFile = DownloadUtils.download(conf.linksUrlZip, zipDest)
         val linksZip = ZipFile(zipFile)
@@ -56,7 +58,7 @@ class CoreEvaluator(private val conf: Configuration, private val rdfEndpoint: St
      * The recommendations will be ordered (desc) after the double value (representing how good the recommendation fits to the source)
      * Thus the best recommendation will be added first, the score for that recommendation will be calculated added to the ROC curve and so on.
      *
-     * The source model will be read in memory to generate correct and false facts to be used to execute against Copaal.
+     * The source model will be read in memory to generate correct and false facts to be used to execute against a Scorer like Copaal.
      *
      *
      * @param source The url of the source string (if locally use file:// as the schema)
@@ -85,16 +87,73 @@ class CoreEvaluator(private val conf: Configuration, private val rdfEndpoint: St
         return normalizer*(roc.calculateAUC()-baseline)
     }
 
+    /**
+     * Creates a ROC based upon if the score got better with the next recommendation
+     *
+     * E.g. lets look at the scores [baseline: 0.1, +recom1: 0.2, +recom2: 0.2, +recom3: 0.3, , +recom4: 0.3]
+     * the scores got better with the recommendations 1 and recommendations 3
+     * hence the score will go up there and to the right on the addition of recommendation 2 and 4
+     *
+     * The ROC will look like:
+     * ROC[(0, 0.25), (0.25, 0.25), (0.25, 0.5), (0.5, 0.5)]
+     *
+     * Afterwards we can normalize the ROC to
+     * ROC[(0, 0.5), (0.5, 0.5), (1.0, 0.5), (1.0, 1.0)]
+     */
+    fun getBetterROC(source: String, facts: List<Pair<Statement, Double>>, recommendations: MutableList<Pair<String, Double>>): ROCCurve {
+        // sort recommendations, s.t. highest recommendation value is on top/first
+        recommendations.sortByDescending { it.second }
+        //We will store only the directions we want to go so we can calculate the upwards and rightwards steps in the ROC later
+        val directions = mutableListOf<DIRECTION>()
+        var counter = 1.0
+        var oldScore = 0.0
+        for((dataset, _) in recommendations){
+            if (counter>maxRecommendations && maxRecommendations>0){
+                //if maxRecommendations > 0 and the the TOP N datasets were checked, break and return
+                break
+            }
+            println("[*] Current dataset to add %s".format(dataset))
+            val score = getScore(facts, source, dataset)
+            println("[+] %s added, Score: %f".format(dataset, score))
 
-    //TODO clean me up
+            //store the directions
+            // if the score didn't get better, go right, otherwise go up
+            if(score > oldScore){
+                directions.add(DIRECTION.UP)
+            }else{
+                directions.add(DIRECTION.RIGHT)
+            }
+            oldScore = score
+            counter += 1.0
+        }
+        //now we can calculate the upwards steps and rightwards steps
+        val upCount = directions.count {it == DIRECTION.UP}
+        val rightCount = directions.count {it == DIRECTION.RIGHT}
+        //and create our ROC curve
+        val roc =ROCCurve(upCount, rightCount)
+        //now we can simply say go up and right each time the score got better (resp. didn't get better) and will land at Point(1.0, 1.0)
+        directions.forEach {
+            if(it == DIRECTION.UP){
+                roc.addUp()
+            }else{
+                roc.addRight()
+            }
+        }
+        return roc
+    }
+        //TODO clean me up
     //FIXME the ROC curve can still get worse
     fun getROC(source: String, facts: List<Pair<Statement, Double>>, recommendations: MutableList<Pair<String, Double>>): ROCCurve{
         recommendations.sortByDescending { it.second }
         //steps doesn't ,matter in our case, we don't know the first either way
-        val roc = ROCCurve(0, recommendations.size)
+        var maxSize = recommendations.size
+        if(maxRecommendations>0) {
+            maxSize = recommendations.size.coerceAtMost(maxRecommendations)
+        }
+        val roc = ROCCurve(0, maxSize)
 
         var counter=1.0
-        for((dataset, value) in recommendations){
+        for((dataset, _) in recommendations){
             if (counter>maxRecommendations && maxRecommendations>0){
                 //if maxRecommendations > 0 and the the TOP N datasets were checked, break and return
                 return roc
@@ -103,7 +162,7 @@ class CoreEvaluator(private val conf: Configuration, private val rdfEndpoint: St
             val score = getScore(facts, source, dataset)
             println("[+] %s added, Score: %f".format(dataset, score))
             //FIXME make use of better functions addUP, ...
-            roc.addPoint(counter/recommendations.size, score)
+            roc.addPoint(counter/maxSize, score)
             counter += 1.0
         }
 
---
+++ src/test/kotlin/org/dice_group/glisten/core/evaluation/CoreEvaluatorTest.kt
@@ -0,0 +1,19 @@
+package org.dice_group.glisten.core.evaluation
+
+class CoreEvaluatorTest {
+
+    //test init
+    // 1. correct download and unzip correctly
+    // 2. no download but IOException because wrong url
+    // 3. download but not zip -> ZipException -> file should be removed
+
+    // get ROC check if correct ROC is created
+    // MockupScorer -> no full functionality, just return some previous defined no.
+    // 1. correct no. of recommendations checked
+    // 2. sine
+
+    //getAUC - this is basically the whole workflow
+    // MockupScript -> check if correct links will be used
+    // MockupScorer -> simply return some scores (no full functionality)
+    // correct normalizing (remove baseline)
+}
