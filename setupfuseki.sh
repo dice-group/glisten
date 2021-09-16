aa5e6285fb62b69dee75c7a1079c759e6fde6d43
TortugaAttack <lixiconrads@gmail.com>
2021-09-16 14:19:39 +0200

    correct fuseki download link

--- setupfuseki.sh
+++ setupfuseki.sh
@@ -2,7 +2,7 @@ wget https://dlcdn.apache.org/jena/binaries/apache-jena-4.2.0.tar.gz
 tar -xzvf apache-jena-4.2.0.tar.gz 
 rm apache-jena-4.2.0.tar.gz
 
-wget https://dlcdn.apache.org/jena/binaries/apache-jena-4.2.0-fuseki.tar.gz
+wget https://dlcdn.apache.org/jena/binaries/apache-jena-fuseki-4.2.0.tar.gz
 tar -xzvf apache-jena-fuseki-4.2.0.tar.gz 
 rm apache-jena-fuseki-4.2.0.tar.gz
 
