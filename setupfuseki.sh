945b86d303395925f0bd985dec4974ccb571643e
TortugaAttack <lixiconrads@gmail.com>
2021-09-20 23:06:38 +0200

    removing tmp source jic

--- setupfuseki.sh
+++ setupfuseki.sh
@@ -4,6 +4,7 @@
 cd /glisten/hdt-java/ && mvn install && cd ..
 cd /glisten/hdt-java/hdt-fuseki/ && mvn package dependency:copy-dependencies
 
+rm /glisten/tmp_source.nt
 rm /glisten/empty.nt
 touch /glisten/empty.nt
 cd /glisten/hdt-java/hdt-java-cli/ && ./bin/rdf2hdt.sh /glisten/empty.nt /glisten/second.hdt
